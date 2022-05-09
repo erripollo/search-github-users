d2eeb9cc3bcd54b75d4cc9cb2ba7c5956b1384c0
erripollo <pollone.enrico@gmail.com>
2022-05-09 22:10:01 +0200

    build: create search-users component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SearchUsersComponent } from './search-users/search-users.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SearchUsersComponent
   ],
   imports: [
     BrowserModule,
